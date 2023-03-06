de12d06cb506bb9269ea1523672b78fe70269ecd
ZayJayPlays <116956084+ZayJayPlays@users.noreply.github.com>
2023-03-05 18:31:29 -0700

    First commit

--- AppAnatomyPractice/ViewController.swift
+++ AppAnatomyPractice/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var labelDidFinishLaunch: UILabel!
+    var countDidFinishLaunch = 0
+    @IBOutlet var labelConfigurationForConnecting: UILabel!
+    var countConfigurationForConnecting = 0
+    @IBOutlet var labelSceneWillConnect: UILabel!
+    var countSceneWillConnect = 0
+    @IBOutlet var labelSceneDidBecomeActive: UILabel!
+    var countSceneDidBecomeActive = 0
+    @IBOutlet var labelSceneWillResign: UILabel!
+    var countSceneWillResign = 0
+    @IBOutlet var labelSceneWillEnterForeground: UILabel!
+    var countSceneWillEnterForeground = 0
+    @IBOutlet var labelSceneDidEnterBackground: UILabel!
+    var countSceneDidEnterBackground = 0
+    
+    var appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateUI() {
+        labelDidFinishLaunch.text = "App launched \(appDelegate.launchCount) times."
+        labelSceneWillResign.text = "Scene Resigned \(countSceneWillResign) times"
+        labelSceneWillConnect.text = "Scene connected \(countSceneWillConnect) times"
+        labelConfigurationForConnecting.text = "ConfigurationForConnecting count: \(countConfigurationForConnecting)"
+        labelSceneDidBecomeActive.text = "Scene became active \(countSceneDidBecomeActive) times"
+        labelSceneDidEnterBackground.text = "Scene entered background \(countSceneDidEnterBackground) times"
+        labelSceneWillEnterForeground.text = "Scene entered foreground \(countSceneWillEnterForeground) times"
     }
 
 
